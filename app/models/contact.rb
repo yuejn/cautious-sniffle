6c236029f49a11ba2008d838a91e2cada0aaffb6
yuejn <jennifer8ho@gmail.com>
2016-04-11 21:31:30 +0200

    contact test presence validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,4 +7,11 @@ class Contact
   field :email, type: String
   field :phone, type: String
 
+  # Validations
+
+  validates_presence_of :forename
+  validates_presence_of :surname
+  validates_presence_of :email
+  validates_presence_of :phone
+
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
+  
   it "has a valid factory" do
     expect(FactoryGirl.create(:contact)).to be_valid
   end
-  it "is invalid without a forename"
-  it "is invalid without a surname"
-  it "is invalid without an email"
-  it "is invalid without a phone number"
+
+  it "is invalid without a forename" do
+    expect(FactoryGirl.build(:contact, forename: nil)).to_not be_valid
+  end 
+
+  it "is invalid without a surname" do
+    expect(FactoryGirl.build(:contact, surname: nil)).to_not be_valid
+  end 
+
+  it "is invalid without an email" do
+    expect(FactoryGirl.build(:contact, email: nil)).to_not be_valid
+  end 
+
+  it "is invalid without a phone number" do
+    expect(FactoryGirl.build(:contact, phone: nil)).to_not be_valid
+  end 
+
   it "returns a contact's full name as a string"
 end
