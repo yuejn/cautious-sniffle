18ac3e4a93a9492594fdf2591d4c7255933f1a57
yuejn <jennifer8ho@gmail.com>
2016-04-11 21:18:41 +0200

    contact unit test init

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory"
+  it "is invalid without a forename"
+  it "is invalid without a surname"
+  it "is invalid without an email"
+  it "is invalid without a phone number"
+  it "returns a contact's full name as a string"
 end
